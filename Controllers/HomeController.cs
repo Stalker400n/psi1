4ec5e85885c4fa54509278bc7b5898b5cc460aae
unknown <kasij.vit@gmail.com>
2025-09-23 00:42:18 +0300

    Preliminary Team creation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using psi1.Models;
 
 namespace psi1.Controllers;
 public class HomeController : Controller
 {
+    private static List<TeamViewModel> _Teams = new List<TeamViewModel>();
+    private static int _nextTeamId = 1; 
+
     public IActionResult Index()
     {
         return View();
@@ -15,14 +19,28 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpPost]
+    public IActionResult CreateTeam(psi1.Models.TeamViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            model.Id = _nextTeamId++;
+            _Teams.Add(model);
+            return RedirectToAction(nameof(BrowseTeams));
+        }
+        return View(model);
+    }
+
+
     public IActionResult BrowseTeams()
     {
-        return View();
+        return View(_Teams);
     }
 
     public IActionResult JoinTeam()
     {
         return View();
     }
+
 }
 
---
+++ Models/TeamViewModel.cs
@@ -0,0 +1,8 @@
+namespace psi1.Models;
+
+public class TeamViewModel
+{
+    public int Id { get; set; }
+    public string TeamName { get; set; }
+    public string Privacy { get; set; }
+}

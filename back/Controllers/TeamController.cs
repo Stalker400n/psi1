35e20a9e017c386c02ec595d47b9fa2d70dcb356
Kasijus Viteika <kasij.vit@gmail.com>
2025-09-29 14:57:28 +0300

    Added the possibility to view a specific teams settings and change them by Id. Also made git ignore .sln files as it is VSCode specific

--- back/Controllers/TeamController.cs
+++ back/Controllers/TeamController.cs
@@ -22,4 +22,30 @@ public class TeamController : ControllerBase
     Teams.Add(team);
     return CreatedAtAction(nameof(GetTeams), new { id = team.Id }, team);
   }
+
+  [HttpGet("{id}")]
+  public ActionResult<Team> GetTeamById(int id)
+  {
+    var team = Teams.FirstOrDefault(t => t.Id == id);
+    if (team == null)
+    {
+      return NotFound();
+    }
+    return Ok(team);
+  }
+
+  [HttpPut("{id}")]
+  public ActionResult<Team> EditTeamSettings(int id, [FromBody] Team team)
+  {
+    var currentTeam = Teams.FirstOrDefault(t => t.Id == id);
+    if (currentTeam == null)
+    {
+      return NotFound();
+    }
+
+    currentTeam.Name = team.Name;
+    currentTeam.IsPrivate = team.IsPrivate;
+
+    return Ok(currentTeam);
+  }
 }

ec8ce0585ecf25ada76cd6e6092c318ec4c0f71a
Kasijus Viteika <kasij.vit@gmail.com>
2025-12-15 23:41:03 +0200

    Migrations

---
+++ back/Migrations/20251215212857_SyncApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace back.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncApplicationDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlaybackPosition",
+                table: "Teams",
+                newName: "ElapsedSeconds");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartedAtUtc",
+                table: "Teams",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartedAtUtc",
+                table: "Teams");
+
+            migrationBuilder.RenameColumn(
+                name: "ElapsedSeconds",
+                table: "Teams",
+                newName: "PlaybackPosition");
+        }
+    }
+}
